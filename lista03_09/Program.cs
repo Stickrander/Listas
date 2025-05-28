9030bfa794fabd6a5f49e38d123cd08623038ce4
pedro.Henrique .Peixoto <145711896+Stickrander@users.noreply.github.com>
2025-05-28 17:23:11 -0300

    Add files via upload

---
+++ lista03_09/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] par = new int[6];
+            int num;
+
+            for (int i = 0; i < par.Length; i++)
+            {
+                Console.Write($"Informe o valor da posição {i + 1}: ");
+                num = int.Parse(Console.ReadLine());
+
+                while (num % 2 != 0)
+                {
+                    Console.Write($"Informe o valor da posição {i + 1}: ");
+                    num = int.Parse(Console.ReadLine());
+                }
+
+                if (num % 2 == 0)
+                {
+                    par[i] = num;
+                }
+
+
+            }
+
+            Console.Write("\nVetor original: ");
+
+            for (int i = 0; i < par.Length; ++i)
+            {
+                Console.Write($"{par[i]} ");
+            }
+
+            Console.Write("\nVetor inverso: ");
+
+            for (int i = par.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{par[i]} ");
+            }
+        }
+    }
+}
---
+++ lista03_10/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista03_10
+{
+    internal class Program
+    {
+
+        static int [] Prenchervetor(int[] vet)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < vet.Length; i++)
+            {
+
+                //Console.Write($"Digite a nota do {i+1}º Aluno: ");
+                //vet[i] = int.Parse(Console.ReadLine());
+
+                vet[i] = rnd.Next(1,50);
+
+                
+            }
+
+            return vet;
+        }
+
+        static int Media(int[] vet)
+        {
+            int soma = 0;
+            
+            for (int i = 0; i < vet.Length; i++)
+            {
+                soma += vet[i];
+            }
+
+            soma = soma / vet.Length;
+
+            return soma;
+        }
+
+        static void Main(string[] args)
+        {
+
+            /*10. Faça um programa para ler a nota da prova de 15 alunos e armazene num vetor. Use um
+método para calcular e exibir a média geral.*/
+
+
+            int[] vet = new int[15];
+            int soma;
+            
+
+            Console.WriteLine("Vetor Prenchido");
+            Prenchervetor (vet);
+            for (int i = 0; i < vet.Length; i++) 
+            {
+                Console.Write($"{vet[i]} ");
+            }
+
+            Console.WriteLine();
+            Media(vet);
+            soma = Media(vet);
+            Console.WriteLine($"A media geral dos alunos e: {soma}");
+
+            Console.WriteLine();
+        }
+    }
+}
