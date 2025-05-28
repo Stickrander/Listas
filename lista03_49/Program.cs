0084e8950fcf8fa628fd4c7686bd5e184013e423
pedro.Henrique .Peixoto <145711896+Stickrander@users.noreply.github.com>
2025-05-28 18:11:41 -0300

    Update Program.cs

--- lista03_49/Program.cs
+++ lista03_49/Program.cs
@@ -1,15 +1,63 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lista03_49
+namespace ConsoleApp10Matriz
 {
-    internal class Program
+    class Program
     {
+        static void ImprimirMatriz(int[,] matriz)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void PreencherMatriz(int[,] matriz)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = random.Next(10, 100);
+                }
+            }
+        }
+
+        static double DiagonalPrincipal(int[,] matriz)
+        {
+            double soma = 0;
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        soma = soma + matriz[i, j];
+                    }
+                }
+            }
+            return soma;
+        }
         static void Main(string[] args)
         {
+            int[,] matriz = new int[3, 3];
+            double soma;
+
+            PreencherMatriz(matriz);
+            Console.WriteLine();
+            ImprimirMatriz(matriz);
+            soma = DiagonalPrincipal(matriz);
+
+            Console.WriteLine($"A soma dos elementos da diagonal principal é {soma}");
         }
     }
 }
