43a194f3a3551036b1cb4265af26edcc90f47b61
pedro.Henrique .Peixoto <145711896+Stickrander@users.noreply.github.com>
2025-05-28 18:08:43 -0300

    Update Program.cs

--- lista03_43/Program.cs
+++ lista03_43/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lista03_43
-{
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            int[,] matriz = new int[4, 4];
+            Random random = new Random();
+            int maiorValor = int.MinValue;
+            int indiceI = 0, indiceJ = 0;
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = random.Next(10, 51);
+                    Console.Write($"{matriz[i,j]}  ");
+
+                    if (matriz[i, j] >  maiorValor)
+                    {
+                        maiorValor = matriz[i, j];
+                        indiceI = i;
+                        indiceJ = j;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Maior valor é {maiorValor} e se encontra no indice [{indiceI},{indiceJ}]");
         }
     }
 }
