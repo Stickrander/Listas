9a14dc823370b61fdf0c6b9fd709019015772818
pedro.Henrique .Peixoto <145711896+Stickrander@users.noreply.github.com>
2025-05-28 18:13:37 -0300

    Update Program.cs

--- lista03_50/Program.cs
+++ lista03_50/Program.cs
@@ -1,15 +1,63 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lista03_50
+namespace ConsoleApp10Matriz
 {
-    internal class Program
+    class Program
     {
+        static void ImprimirMatriz(int[,] matriz)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void PreencherMatriz(int[,] matriz)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = random.Next(10, 100);
+                }
+            }
+        }
+
+        static int [,] Transposta(int[,] matriz)
+        {
+            int[,] matrizT = new int[3, 3];
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matrizT[i, j] = matriz[j, i];
+                }
+            }
+            return matrizT;
+        }
         static void Main(string[] args)
         {
+            int[,] matriz = new int[3, 3];
+            int[,] matrizT;
+
+            Console.WriteLine("Matriz: ");
+            PreencherMatriz(matriz);
+            Console.WriteLine();
+            ImprimirMatriz(matriz);
+            Console.WriteLine("\nMatriz transposta: ");
+            matrizT = Transposta(matriz);
+            ImprimirMatriz(matrizT);
+
         }
     }
 }
